74537a7cb825019909a666a9ee29618e99d4ed8e
Eric Kramer <theerickramer@gmail.com>
2018-07-31 16:39:35 -0400

    added vundle & eslint to install

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@ echo 'source ~/.dotfiles/.alias.sh' >> ~/.bash_profile
 echo 'source ~/.dotfiles/.prompt.sh' >> ~/.bash_profile
 
 # symlinks
-rm ~/.vimrc && ln ~/.dotfiles/.vimrc ~/.vimrc
-rm ~/Library/Application\ Support/Code/User/settings.json && ln ~/.dotfiles/.vs-code-settings.json ~/Library/Application\ Support/Code/User/settings.json
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+rm ~/.vimrc
+rm ~/Library/Application\ Support/Code/User/settings.json 
+ln ~/.dotfiles/.vimrc ~/.vimrc
+ln ~/.dotfiles/.vs-code-settings.json ~/Library/Application\ Support/Code/User/settings.json
 
 # global npm packages
 brew install yarn
 yarn global add browser-sync
-
+yarn global add eslint
