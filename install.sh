05f2f8fcf420713373c0caeff86cdf2fe2a31cd1
Eric Kramer <theerickramer@gmail.com>
2018-07-31 09:51:39 -0400

    fixed application support path

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo 'source ~/.dotfiles/.prompt.sh' >> ~/.bash_profile
 
 # symlinks
 rm ~/.vimrc && ln ~/.dotfiles/.vimrc ~/.vimrc
-rm ~/Library/Application Support/Code/User/settings.json && ln ~/.dotfiles/.vs-code-settings.json ~/Library/Application Support/Code/User/settings.json
+rm ~/Library/Application\ Support/Code/User/settings.json && ln ~/.dotfiles/.vs-code-settings.json ~/Library/Application\ Support/Code/User/settings.json
 
 # global npm packages
 brew install yarn
