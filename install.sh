5346dfc363debe92ca4f15904e4d19e46925c5be
ERIC KRAMER <erickramer@ERICs-MacBook-Air.local>
2019-09-06 10:15:32 -0400

    updates

--- install.sh
+++ install.sh
@@ -2,14 +2,22 @@
 echo 'source ~/.dotfiles/.alias.sh' >> ~/.bash_profile
 echo 'source ~/.dotfiles/.prompt.sh' >> ~/.bash_profile
 
-# symlinks
+# Vundle & symlinks
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 rm ~/.vimrc
 rm ~/Library/Application\ Support/Code/User/settings.json 
 ln ~/.dotfiles/.vimrc ~/.vimrc
 ln ~/.dotfiles/.vs-code-settings.json ~/Library/Application\ Support/Code/User/settings.json
 
+# webApi-vim (necessary for emmet)
+cd ~/.dotfiles && git clone git@github.com:mattn/webapi-vim.git
+cp -r  ~/.dotfiles/webapi-vim/autoload/ ~/.vim/autoload
+
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # global npm packages
 brew install yarn
 yarn global add browser-sync
 yarn global add eslint
+
