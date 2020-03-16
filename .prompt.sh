7960c40a6632dc558a553468bd03bbe0303345c6
Eric Kramer <theerickramer@gmail.com>
2020-03-16 11:55:00 -0400

    git show dirty

--- .prompt.sh
+++ .prompt.sh
@@ -1,3 +1,4 @@
 # prompt colors and git branch
 source ~/.dotfiles/.git-prompt.sh
 PS1='\[\e[0;92m\]\h: \[\e[1;93m\]\W \[\e[0;95m\]\u\[\e[1;91m\]$(__git_ps1) \[\e[0;92m\]$\[\e[0;96m\] ' 
+GIT_PS1_SHOWDIRTYSTATE=true
